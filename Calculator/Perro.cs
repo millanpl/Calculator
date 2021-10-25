b4f97bc0f9f4df67d31c98e8940f068aef0f666e
Millan <millan.peiro@atos.net>
2021-10-25 19:01:25 +0200

    Add project files.

---
+++ Calculator/Perro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Perro
+    {
+        public string Ladrar()
+        {
+            return "Guau!!!";
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Perro fido = new Perro();
+            Console.WriteLine(fido.Ladrar());
+            return 0;
+        }
+    }
+}
